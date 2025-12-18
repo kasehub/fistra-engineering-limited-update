9793d99a52acf840a3215553ade4d8a36cb140aa
kasehub <168753276+kasehub@users.noreply.github.com>
2025-12-18 12:00:05 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+function loadHTML(id, file) {
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(id).innerHTML = data;
+      });
+  }
+  
+  loadHTML("header", "header.html");
+  loadHTML("footer", "footer.html");
+  
+     
      // Mobile menu functionality
      const menuToggle = document.getElementById('menu-toggle');
      const nav = document.getElementById('nav');
